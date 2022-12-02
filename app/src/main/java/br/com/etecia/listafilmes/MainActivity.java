2ca49d5de6732440c79ef6da727ea3552a4888ce
AndreNeves30 <andre.neves30@etec.sp.gov.br>
2022-12-01 22:08:33 -0300

    oi2

--- app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/listafilmes/MainActivity.java
@@ -7,10 +7,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ListView listaFilmes;
+
+    String titulo[] = {"Shrek","O Senhor dos Anéis: A Sociedade do Anel","Os Croods","Kung Fu Panda 3","Bob Esponja: Um Herói Fora d'Água","Como Treinar Seu Dragão 2"};
+    String descricao[] = {"Um ogro tem sua vida invadida por personagens de contos de fadas que acabam com a tranquilidade de seu lar. Ele faz um acordo pra resgatar uma princesa.","Na primeira parte, O Senhor dos Anéis, um jovem e tímido hobbit chamado Frodo Bolseiro herda um simples anel de ouro que guarda o segredo da sobrevivência – ou escravidão – de todo o mundo.","Um homem das cavernas (Nicolas Cage) e sua família procuram um novo lar quando a Mãe Natureza se torna hostil.","Po treina uma vila de pandas para lutar contra um inimigo sobrenatural.","Em uma missão para salvar seu mundo, Bob Esponja Calça Quadrada está indo para o nosso pela primeira vez!","Este emocionante segundo capítulo da trilogia Como Treinar o Seu Dragão retorna ao mundo fantástico de Soluço e Banguela enquanto eles se unem para salvar o futuro de homens e dragões!"};
+    String acesso[] = {"4.7","4.9","4.7","4.4","4.7","4.8"};
+    int filmes[] = {R.drawable.sherek,R.drawable.lotr,R.drawable.croods,R.drawable.po,R.drawable.bob,R.drawable.soluco};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
 
         listaFilmes.setAdapter(adapter);
     }
-    //criando uma inner class
 
     public class MyAdapter extends BaseAdapter{
 
@@ -44,7 +50,24 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            return null;
+
+            ImageView imgfilme;
+            TextView titulofilme, descricaofilme , acessofilme;
+
+            View v = getLayoutInflater().inflate(R.layout.modelo_filmes,null);
+
+            imgfilme = v.findViewById(R.id.imgFilme);
+            titulofilme = v.findViewById(R.id.txttituloFilme);
+            descricaofilme = v.findViewById(R.id.txtdescricaoFilmes);
+            acessofilme = v.findViewById(R.id.txtacessoFilme);
+
+            titulofilme.setText(titulo[i]);
+            descricaofilme.setText(descricao[i]);
+            acessofilme.setText(acesso[i]);
+            imgfilme.setImageResource(filmes[i]);
+
+
+            return v;
         }
     }
 }
